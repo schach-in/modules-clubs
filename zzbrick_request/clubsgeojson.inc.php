b6a8df19eac91ecc5588ef9dfb7bd019598e0c23
Gustaf Mossakowski <gustaf@koenige.org>
2022-05-09 15:24:07 +0200

    create separate request function clubsgeojson for GeoJSON output; base clubs, clubsgeojson request functions on new common request/get-function extracted from clubs

---
+++ zzbrick_request/clubsgeojson.inc.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * clubs module
+ * output GeoJSON data for organisations
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/clubs
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2015-2022 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * output GeoJSON data for clubs, schools, etc.
+ *
+ * @param array $params
+ * @return array $page
+ */
+function mod_clubs_clubsgeojson($params, $settings = []) {
+	if (count($params) !== 1) return false;
+	if (str_ends_with($params[0], '.geojson'))
+		$params[0] = substr($params[0], 0, -8);
+
+	$source = brick_request_data('clubs', $params, $settings);
+	if (!$source['coordinates']) return false;
+
+	$page['content_type'] = 'geojson';
+	$page['query_strings'][] = 'q';
+	$page['ending'] = 'none';
+	$page['headers']['filename'] = sprintf('%s.geojson', $params[0]);
+
+	$conditional_properties = [
+		'members', 'u25', 'female', 'avg_age', 'avg_rating'
+	];
+	$data = [];
+	$data['type'] = 'FeatureCollection';
+	foreach ($source['coordinates'] as $index => $coordinate) {
+		$properties = [
+			'org' => $coordinate['title'],
+			'identifier' => $coordinate['identifier'],
+			'category' => $coordinate['category'],
+			'awards' => intval($coordinate['auszeichnungen']),
+		];
+		foreach ($conditional_properties as $prop) {
+			if (!$coordinate[$prop]) continue;
+			$properties[$prop] = intval($coordinate[$prop]);
+		}
+		$data['features'][] = [
+			'type' => 'Feature',
+			'id' => $index,
+			'properties' => $properties,
+			'geometry' => [
+				'type' => 'Point',
+				'coordinates' => [
+					floatval($coordinate['y_longitude']),
+					floatval($coordinate['x_latitude'])
+				]
+			]
+		];
+	}
+	$page['text'] = json_encode($data);
+	return $page;
+}
