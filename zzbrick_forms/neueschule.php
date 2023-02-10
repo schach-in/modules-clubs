98566562789148caa34232360fbda1722c6945c4
Gustaf Mossakowski <gustaf@koenige.org>
2023-02-10 17:53:45 +0100

    school: update add school form, translate to English; base on contacts/contacts table script; directly include address, not via other contact

--- zzbrick_forms/neueschule.php
+++ zzbrick_forms/neueschule.php
@@ -14,50 +14,74 @@
 
 
 $zz_setting['cache'] = false;
-require $zz_conf['form_scripts'].'/organisationen.php';
+
+$values['contactdetails_restrict_to'] = 'school';
+$values['relations_restrict_to'] = 'school';
+$zz = zzform_include_table('contacts/contacts', $values);
+
+$zz['title'] = 'Add school chess group';
 
 if (empty($_SESSION['login_id'])) {
 	$zz['revisions_only'] = true;
 }
 
-$zz['title'] = 'Schulschachgruppen';
-$zz['access'] = 'add_then_edit';
+// contact
+$zz['fields'][2]['title'] = 'Name of the school';
 
-$zz['fields'][3]['title'] = 'Name der Schule';
+// identifier
+$zz['fields'][3]['hide_in_form'] = true;
 
-$zz['fields'][11]['title'] = 'Über uns';
-
-$zz['fields'][6]['type'] = 'hidden';
-$zz['fields'][6]['class'] = 'hidden';
-$zz['fields'][6]['value'] = wrap_category_id('contact/school');
-
-unset($zz['fields'][34]);
-unset($zz['fields'][4]);
-
-$zz['fields'][9]['title'] = 'Land';
-
-$zz['fields'][24] = zzform_include_table('../zzbrick_forms/places');
-unset($zz['fields'][24]['fields'][24]);
-$zz['fields'][24]['type'] = 'subtable';
-$zz['fields'][24]['form_display'] = 'inline';
-$zz['fields'][24]['list_display'] = 'inline';
-$zz['fields'][24]['title'] = 'Ort';
-$zz['fields'][24]['min_records'] = 1;
-$zz['fields'][24]['min_records_required'] = 1;
-$zz['fields'][24]['max_records'] = 1;
-unset($zz['fields'][24]['explanation']);
-//$zz['fields'][24]['fields'][25]['type'] = 'foreign_key';
-
-unset($zz['fields'][13]);
-unset($zz['fields'][2]);
-$zz['fields'][7]['class'] = 'hidden';
-unset($zz['fields'][5]);
-
-unset($zz['fields'][40]['title_append']);
-$zz['fields'][40]['append_next'] = false;
-
+// contact_short
 unset($zz['fields'][10]);
-unset($zz['fields'][12]);
-unset($zz['fields'][15]);
 
+// contact_category_id
+$zz['fields'][4]['type'] = 'hidden';
+$zz['fields'][4]['class'] = 'hidden';
+$zz['fields'][4]['value'] = wrap_category_id('contact/school');
+
+// address
+$zz['fields'][5]['min_records'] = 1;
+$zz['fields'][5]['max_records'] = 1;
+
+$zz['fields'][5]['fields'][6]['type'] = 'hidden';
+$zz['fields'][5]['fields'][6]['type_detail'] = 'select';
+$zz['fields'][5]['fields'][6]['value'] = wrap_id('countries', 'DE');
+
+$zz['fields'][5]['fields'][9]['type'] = 'hidden';
+$zz['fields'][5]['fields'][9]['type_detail'] = 'select';
+$zz['fields'][5]['fields'][9]['value'] = wrap_category_id('address/work');
+$zz['fields'][5]['fields'][9]['hide_in_form'] = true;
+
+// 30+ contactdetails
+
+// descripton
+$zz['fields'][12]['title'] = 'About us';
+
+// start_date
+$zz['fields'][16]['hide_in_form'] = true;
+
+// end_date
+$zz['fields'][17]['hide_in_form'] = true;
+
+// country_id
+$zz['fields'][18]['title'] = 'State';
+$zz['fields'][18]['sql'] = 'SELECT country_id, country, main_country_id
+	FROM countries
+	ORDER BY country_code3';
+$zz['fields'][18]['character_set'] = 'latin1';
+$zz['fields'][18]['show_hierarchy'] = 'main_country_id';
+$zz['fields'][18]['show_hierarchy_subtree'] = wrap_id('countries', 'DE');
+
+// remarks
+$zz['fields'][13]['hide_in_form'] = true;
+
+// published
+$zz['fields'][14]['hide_in_form'] = true;
+$zz['fields'][14]['type'] = 'hidden';
+$zz['fields'][14]['value'] = 'no';
+
+// parameters
+$zz['fields'][15]['hide_in_form'] = true;
+
+$zz['access'] = 'add_then_edit';
 $zz_conf['no_timeframe'] = true;
