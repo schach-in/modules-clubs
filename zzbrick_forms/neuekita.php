ce5b32a729748a78baf3dd42d9c9ad985d0c7ef4
Gustaf Mossakowski <gustaf@koenige.org>
2023-04-27 10:27:09 +0200

    neuekita form: use zzform_include_table()

--- zzbrick_forms/neuekita.php
+++ zzbrick_forms/neuekita.php
@@ -8,16 +8,15 @@
  * https://www.zugzwang.org/modules/clubs
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2017, 2021 Gustaf Mossakowski
+ * @copyright Copyright © 2017, 2021, 2023 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
 
-require $zz_conf['form_scripts'].'/organisationen.php';
+$zz = zzform_include_table('organisationen'); // @todo use contacts directly
 
-if (empty($_SESSION['login_id'])) {
+if (empty($_SESSION['login_id']))
 	$zz['revisions_only'] = true;
-}
 
 $zz['access'] = 'add_then_edit';
 
