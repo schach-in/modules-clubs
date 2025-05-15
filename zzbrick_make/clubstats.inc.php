f59c0c81de09adcfbe92f78173345dbc33d11c9a
Gustaf Mossakowski <gustaf@koenige.org>
2025-05-15 22:01:42 +0200

    clubstats: send mails with added and deleted clubs after each update

--- zzbrick_make/clubstats.inc.php
+++ zzbrick_make/clubstats.inc.php
@@ -54,7 +54,13 @@ function mod_clubs_make_clubstats() {
 			, ROUND(SUM(IF(DWZ != 0, DWZ, 0)) / IF(SUM(IF(DWZ != 0, 1, 0)), SUM(IF(DWZ != 0, 1, 0)), 1)) AS avg_rating
 		FROM dwz_spieler
 		LEFT JOIN contacts_identifiers
-			ON IF(FIND_IN_SET(SUBSTRING(dwz_spieler.ZPS, 1, 1), "/*_SETTING ratings_dsb_federations_are_clubs _*/"), SUBSTRING(dwz_spieler.ZPS, 1, 3), IF(SUBSTRING(dwz_spieler.ZPS, 4, 2) = "00", SUBSTRING(dwz_spieler.ZPS, 1, 3), dwz_spieler.ZPS)) = contacts_identifiers.identifier
+			ON IF(
+				FIND_IN_SET(SUBSTRING(dwz_spieler.ZPS, 1, 1), "/*_SETTING ratings_dsb_federations_are_clubs _*/")
+				, SUBSTRING(dwz_spieler.ZPS, 1, 3)
+				, IF(SUBSTRING(dwz_spieler.ZPS, 4, 2) = "00"
+					, SUBSTRING(dwz_spieler.ZPS, 1, 3)
+					, dwz_spieler.ZPS)
+				) = contacts_identifiers.identifier
 			AND contacts_identifiers.current = "yes"
 			AND contacts_identifiers.identifier_category_id = /*_ID categories identifiers/pass_dsb _*/
 		GROUP BY contact_id';
@@ -65,7 +71,72 @@ function mod_clubs_make_clubstats() {
 	$sql = 'ALTER TABLE `clubstats` ADD UNIQUE `contact_id` (`contact_id`)';
 	wrap_db_query($sql);
 
+	mod_clubs_make_clubstats_new();
+	mod_clubs_make_clubstats_deleted();
+
 	$data['done'] = true;
 	$page['text'] = wrap_template('clubstats', $data);
 	return $page;
+}
+
+/**
+ * check if there are new clubs
+ *
+ * read from dwz_spieler instead of dwz_vereine to check if there are already members
+ *
+ */
+function mod_clubs_make_clubstats_new() {
+	$sql = 'SELECT DISTINCT ZPS AS code, Vereinname AS club
+		FROM dwz_spieler
+		LEFT JOIN dwz_vereine USING (ZPS)
+		LEFT JOIN contacts_identifiers
+			ON IF(
+				FIND_IN_SET(SUBSTRING(dwz_spieler.ZPS, 1, 1), "/*_SETTING ratings_dsb_federations_are_clubs _*/")
+				, SUBSTRING(dwz_spieler.ZPS, 1, 3)
+				, IF(SUBSTRING(dwz_spieler.ZPS, 4, 2) = "00"
+					, SUBSTRING(dwz_spieler.ZPS, 1, 3)
+					, dwz_spieler.ZPS)
+				) = contacts_identifiers.identifier
+			AND contacts_identifiers.current = "yes"
+			AND contacts_identifiers.identifier_category_id = /*_ID categories identifiers/pass_dsb _*/
+		WHERE ISNULL(contact_id)';
+	$data = wrap_db_fetch($sql, 'code');
+	if (!$data) return false;
+	$data = array_values($data); // numerical keys
+
+	$mail = [
+		'to' => [
+			'name' => wrap_setting('project'),
+			'e_mail' => wrap_setting('own_e_mail')
+		],
+		'message' => wrap_template('clubstats-new-mail', $data)
+	];
+	$success = wrap_mail($mail);
+	if (!$success)
+		wrap_error('Unable to send mail: '.json_encode($mail));
+}
+
+/**
+ * check if there are active clubs with no members
+ *
+ */
+function mod_clubs_make_clubstats_deleted() {
+	$sql = 'SELECT DISTINCT ZPS AS code, Vereinname AS club
+		FROM dwz_vereine
+		LEFT JOIN dwz_spieler USING (ZPS)
+		WHERE ISNULL(dwz_spieler.PID)';
+	$data = wrap_db_fetch($sql, 'code');
+	if (!$data) return false;
+	$data = array_values($data); // numerical keys
+
+	$mail = [
+		'to' => [
+			'name' => wrap_setting('project'),
+			'e_mail' => wrap_setting('own_e_mail')
+		],
+		'message' => wrap_template('clubstats-deleted-mail', $data)
+	];
+	$success = wrap_mail($mail);
+	if (!$success)
+		wrap_error('Unable to send mail: '.json_encode($mail));
 }
