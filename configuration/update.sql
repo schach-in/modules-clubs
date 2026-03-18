4140b2cea9638729186ad49caff5700f9b827d8a
Gustaf Mossakowski <gustaf@koenige.org>
2026-03-18 12:19:16 +0100

    routes: add routes.cfg, move paths from access.cfg, settings.cfg to routes.cfg

--- configuration/update.sql
+++ configuration/update.sql
@@ -6,7 +6,7 @@
  * https://www.zugzwang.org/modules/clubs
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2023-2024 Gustaf Mossakowski
+ * @copyright Copyright © 2023-2024, 2026 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -14,3 +14,5 @@
 /* 2023-03-22-1 */	UPDATE _settings SET setting_key = 'club_stats_min_members' WHERE setting_key = 'clubs_statistik_min_mitglieder';
 /* 2023-03-28-1 */	UPDATE _settings SET setting_key = 'clubs_stats_min_members' WHERE setting_key = 'club_stats_min_members';
 /* 2024-03-14-1 */	ALTER TABLE `wochentermine` ADD INDEX `place_contact_id` (`place_contact_id`), DROP INDEX `ort_id`;
+/* 2026-03-18-1 */	DELETE FROM _settings WHERE setting_key = 'clubs_edit_path';
+/* 2026-03-18-2 */	DELETE FROM _settings WHERE setting_key = 'clubs_geojson_path';
