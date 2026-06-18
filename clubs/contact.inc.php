f3054e5211fbe1972ba6898bb5e73a8c242f115f
Gustaf Mossakowski <gustaf@koenige.org>
2026-06-18 12:45:39 +0200

    contact: show average age, not average birth year per club

--- clubs/contact.inc.php
+++ clubs/contact.inc.php
@@ -8,14 +8,14 @@
  * https://www.zugzwang.org/modules/clubs
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2024-2025 Gustaf Mossakowski
+ * @copyright Copyright © 2024-2026 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
 
 function mf_clubs_contact($data, $ids) {
 	$sql = 'SELECT contact_id, members, members_female, members_u25, members_passive
-			, avg_byear, avg_rating
+			, (YEAR(CURDATE()) - avg_byear) AS avg_age, avg_rating
 	    FROM clubstats
 	    WHERE contact_id IN (%s)';
 	$sql = sprintf($sql, implode(',', $ids));
